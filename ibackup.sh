b83d900c8230f7e6e9fd1aeba76a70c03fc37dde
Raghav Gade <gaderaghav5@gmail.com>
2018-10-05 18:45:01 +0530

    Resolved an issue.
    Checks weather parameters are given to the script or not.

--- ibackup.sh
+++ ibackup.sh
@@ -1,13 +1,17 @@
-#! /bin/bash
+#!/bin/bash
 
-# whatever you type here are considered as commands. Hence, if you type here a directory, it tries to take it as a command and says, it's a directory and not a command.
-# $1 and $2 are always parameters. Even if you disable, it will work.
-# $1
-# $2
+if [ $# -eq 0 ]
+then
+	echo " No arguments supplied "
+
+else
 
 var1=$( tar -cvvf backup0.tar -g snap-i $1)
 var2=$( gzip backup0.tar )
 var3=$( mv backup0.tar.gz snap-i $2)
+
+
+fi
 
 echo $#
 echo $1 
