63424220782baa7d53d95b16f63d30c4ba098396
Raj Kiran Gade <raj@redlock.io>
2018-10-07 12:51:47 +0530

    pointers example

--- sum.c
+++ sum.c
@@ -1,22 +1,32 @@
 #include<stdio.h>
-int sum(int x,int y)
+int sum(int * x,int * y)
 {
         int s;
-	s=x+y;
-
-printf("%d",&x);
+	s=*x+*y;
+   	printf("\n Address of x is : %p", x);
+  	printf("\n Address of y is : %p", y);
+        return s;
 }
 
 int main()
 {
 
 	int su,a,b;
-printf("enter two numbers");
-scanf("%d%d",&a,&b);
+        int * pointer_a;
+   	int * pointer_b;
+	pointer_a = &a;
+	pointer_b = &b;
+  	
+	
 
-su=sum(a,b);
-int *f=&a;
-printf("The sum is :%d",su);
+printf("enter two numbers \n ");
+scanf("%d %d",&a,&b);
 
-printf("\n %d \n",&a);
+
+   	printf("\n Address of a is : %p", &a);
+  	printf("\n Address of b is : %p", &b);
+
+su=sum(pointer_a,pointer_b);
+printf("\n The sum is :%d \n \n ",su);
+
 }	
